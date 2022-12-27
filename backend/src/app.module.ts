fbc48895068e6ac914faafb45f65ef6bfb632345
komiyama029 <komiyama@ekanlab.co.jp>
2022-12-27 13:32:14 +0900

    ユーザー新規登録バックエンド実装

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TodosModule } from './todos/todos.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TodosModule, TypeOrmModule.forRoot()],
+  imports: [TodosModule, TypeOrmModule.forRoot(), AuthModule],
   controllers: [],
   providers: [],
 })
